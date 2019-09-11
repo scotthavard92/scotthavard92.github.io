6f6ce460522574d8bfc191775df48ddf0b280d15
Scott Havard <scotthavard92@gmail.com>
2019-09-11 14:08:26 -0700

    Feedback Updates

---
+++ Vote_iFrame/feedbackScript.js
@@ -0,0 +1,50 @@
+
+
+
+function formCollect() {
+	var userFeedback = prompt("We are sorry this didn't help! Can you share what you do not like?", "");
+
+	if (userFeedback == null || userFeedback == "") {
+	  txt = "User cancelled the prompt.";
+	} 
+	else {
+	  window.alert("Thank you for your feedback!");
+	  return userFeedback;
+	}
+}
+
+function zapRequest(feedback, pageTitle) {
+	var webhookURL = "https://hooks.zapier.com/hooks/catch/4543001/o3wjgjx/";
+	var requestBuild = new XMLHttpRequest();
+	var requestBody = payload={
+		"Page Title": pageTitle,
+		"Feedback": feedback 
+	};
+
+	requestBuild.open("POST", webhookURL,true);
+  	requestBuild.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  	requestBuild.send(JSON.stringify(requestBody));
+}
+
+function bindEvent(element, eventName, eventHandler) {
+        if (element.addEventListener){
+            element.addEventListener(eventName, eventHandler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent('on' + eventName, eventHandler);
+        }
+    }
+
+function pageTitle() {
+	var pageTitle;
+    bindEvent(window, 'message', function (e) {
+        pageTitle = e.data;
+        results.innerHTML = pageTitle;
+    });
+    return pageTitle;
+}
+
+// Send a message to the parent
+var sendMessage = function (msg) {
+    // Make sure you are sending a string, and to stringify JSON
+    window.parent.postMessage(msg, '*');
+};
